ee220adcd0da0725ff107778f48924509602c674
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-10-12 00:09:44 +0900

    Interfaceの設定

---
+++ src/app/interfaces/iapi.ts
@@ -0,0 +1,8 @@
+export interface InterfaceApi {
+    repositoryName: string;
+    url: string;
+    description: string;
+    language: string;
+    ownerAvatorUrl: string;
+    ownerLoginName: string;
+}
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { InterfaceApi } from '../interfaces/iapi';
 
-const apiRootUrl = '';    //
+
+const ApiRootUrl = 'http://localhost:3000';    //
+const ApiPath = '/api/favorite';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  postfavorite(): Observable<any> {
-    const body = {};      // JSONファイルで渡す
-    return this.http.post(apiRootUrl, body);
+  postfavorite(postData: InterfaceApi): Observable<any> {
+    const body = postData as InterfaceApi;      // JSONファイルで渡す
+    return this.http.post(ApiRootUrl, body);
   }
 }
